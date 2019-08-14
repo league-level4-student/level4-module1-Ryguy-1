9ad38c6c54b8a6ac1361cd2e8fd8cc6264f2b258
Ryguy-1 <Ryguy-1@users.noreply.github.com>
2019-08-14 16:31:11 -0700

    working on testAddAt not working right look at error

--- src/_01_Custom_ArrayList/ArrayList.java
+++ src/_01_Custom_ArrayList/ArrayList.java
@@ -7,23 +7,71 @@ public class ArrayList <T>{
 	T[] al;
 	
 	public ArrayList() {
-		al = (T[]) new Object();
+		al = (T[]) new Object[0];
 	}
 	
 	public T get(int loc) throws IndexOutOfBoundsException {
 		
-		return null;
+		try {
+			return al[loc];
+		}catch(IndexOutOfBoundsException e) {
+			System.out.println("Out of Bounds!");
+			throw e;
+		}
+		
 	}
 	
 	public void add(T val) {
 		
+		T[] temp = (T[]) new Object[al.length+1];
+		for (int i = 0; i < temp.length; i++) {
+			if(i<al.length) {
+				temp[i]=al[i];
+			}else if(i==al.length) {
+				temp[i]=val;
+			}
+		}
+		al=temp;
 	}
 	
 	public void insert(int loc, T val) throws IndexOutOfBoundsException {
+		T temp = null;
+		T[] testArray = (T[]) new Object[al.length+1];
+		for (int i = 0; i < testArray.length-1; i++) {
+			testArray[i]=al[i];
+		}
+		try {
+			for (int i = 0; i < testArray.length-1; i++) {
+				if(i==loc) {
+					System.out.println(al[i]);
+					temp=testArray[i+1];
+					testArray[i]=val;
+				}else if(i>loc) {
+					System.out.println(testArray[i]);
+					testArray[i]=temp;
+					temp=testArray[i+1];
+				}
+			}
+			al=testArray;
+			
+		}catch(IndexOutOfBoundsException e) {
+			System.out.println("Insert index out of bounds. Use add method.");
+			throw e;
+		}
+			
+		}
 		
-	}
+		
+	
 	
 	public void set(int loc, T val) throws IndexOutOfBoundsException {
+		
+		try {
+			al[loc]=val;
+		}catch(IndexOutOfBoundsException e) {
+			System.out.println("Index out of Bounds");
+			throw e;
+		}
 		
 	}
 	
